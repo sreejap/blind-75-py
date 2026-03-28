9e8a34ffe04493f01adacbd9cb9b4bd5f2a3dbc5
sreejap <sreeja605@gmail.com>
2026-03-28 13:00:07 -0400

    Create 3Sum.py

---
+++ ArraysandStrings/3Sum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        if not nums:
+            return nums
+        nums.sort()
+
+        res = []
+        for i in range (len(nums)):
+            if i==0 or nums[i] != nums[i-1]:
+                self.twoSumII (i,nums,res)
+        return res
+    
+    def twoSumII (self, i, nums:list[int],res:list[list[int]]):
+        left = i+1
+        right = len(nums) -1
+
+        while left < right:
+            total = nums[i] + nums[left] + nums [right]
+
+            if total > 0:
+                right -= 1
+            
+            elif total < 0:
+                left += 1
+
+            else:
+                res.append([nums[i],nums[left],nums[right]])
+                left += 1
+                right -= 1
+                while left < right and nums[left]==nums[left-1]:
+                    left += 1
+                
+                while left < right and nums[right] == nums[right+1]:
+                    right -= 1
+
+        return         
