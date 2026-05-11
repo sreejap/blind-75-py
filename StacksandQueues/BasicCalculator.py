321c4f1858e823dd2a8c74ee528d77a523c335a6
sreejap <sreeja605@gmail.com>
2026-05-11 18:03:04 -0400

    Create BasicCalculator.py

---
+++ StacksandQueues/BasicCalculator.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/basic-calculator/
+class Solution:
+    def calculate(self, s: str) -> int:
+        output, curr, sign, stack = 0, 0, 1, []
+
+        for c in s:
+            if c.isdigit():
+                curr = curr * 10 + int (c)
+            elif c in "+-":
+                output += (curr*sign)
+                curr = 0
+                if c == "-":
+                    sign = -1
+                else:
+                    sign = 1
+            elif c == "(":
+                stack.append(output)
+                stack.append(sign)
+                # reset here
+                output = 0
+                sign = 1
+            elif c == ")":
+                output += (curr*sign) # caclulate the current output
+                output *= stack.pop() # add the remaining from stack
+                output += stack.pop()
+                curr = 0
+            
+        return output + (curr*sign)
