77fea97e670788408c6df427f8e3f8f31cbd447f
sreejap <sreeja605@gmail.com>
2026-05-12 15:47:01 -0400

    Add complexity analysis comments to MergeKSortedLists.py
    Added comments explaining time and space complexity for merging k sorted lists.

--- Heaps/MergeKSortedLists.py
+++ Heaps/MergeKSortedLists.py
@@ -1,3 +1,13 @@
+# https://leetcode.com/problems/merge-k-sorted-lists/
+# Time complexity : O(Nlogk) where k is the number of linked lists.
+
+# The comparison cost will be reduced to O(logk) for every pop and insertion to priority queue. But finding the node with the smallest value just costs O(1) time.
+# There are N nodes in the final linked list.
+# Space complexity :
+
+# O(n) Creating a new linked list costs O(n) space.
+# O(k) The code above present applies in-place method which cost O(1) space. And the priority queue (often implemented with heaps) costs O(k) space (it's far less than N in most situations).
+
 from heapq import heappush, heappop
 # Definition for singly-linked list.
 # class ListNode:
